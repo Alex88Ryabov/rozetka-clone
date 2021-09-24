86fb0690674f4c15f62989ccc52e65e1d67805e4
Alex <ryabovsasha88@gmail.com>
2021-09-24 10:57:49 +0300

    change search styles. added slider.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
-import sayHello from './lib/sayHello';
+import 'slick-carousel';
 
-sayHello();
+$('.slider').slick({
+  autoplay: true,
+  arrows: true,
+  prevArrow: "<button type='button' class='slick-prev pull-left' style='position:absolute; z-index: 2; top: 0; left: 0;width: 24px; height: 100%; background: linear-gradient(-90deg, rgba(255, 255, 255, 0), rgb(255, 255, 255));'><svg width='16' height='40' viewBox='0 0 355 1024'><path fill = '#3e77aa' d = 'M50.258 517.899l301.194 471.812c6.344 9.938 3.974 23.413-5.454 30.305-9.729 7.112-23.094 4.419-29.635-5.827l-316.628-495.991 316.496-508.178c6.427-10.319 19.759-13.176 29.566-6.182 9.506 6.781 12.021 20.229 5.786 30.241l-301.326 483.819z'></path></svg ></button>",
+  nextArrow: "<button type='button' class='slick-next pull-right' style='position:absolute; z-index: 2; top: 0; right: 0;width: 24px; height: 100%; background: linear-gradient(90deg, rgba(255, 255, 255, 0), rgb(255, 255, 255));'><svg width='16' height='40' viewBox='0 0 355 1024'><path fill = '#3e77aa' d = 'M304.742 506.101l-301.194-471.812c-6.344-9.938-3.974-23.413 5.454-30.305 9.729-7.112 23.094-4.419 29.635 5.827l316.628 495.991-316.496 508.178c-6.427 10.319-19.759 13.176-29.566 6.182-9.506-6.781-12.021-20.229-5.786-30.241l301.326-483.819z'></path</svg ></button>"
+});
+    
