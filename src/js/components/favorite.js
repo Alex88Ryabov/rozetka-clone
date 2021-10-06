db36639aae080b3b40059e5c078b266bc50eb2f1
Alex <ryabovsasha88@gmail.com>
2021-10-06 07:40:57 +0300

    added changes to favorite.js

--- src/js/components/favorite.js
+++ src/js/components/favorite.js
@@ -1,38 +1,34 @@
-// const cards = document.querySelectorAll('.products-preview__products');
-// console.log('cards');
-// cards.forEach(card => {
-
-//   const favouritBtn = card.querySelector('.js-favourit');
-
-//   favouritBtn.addEventListener('click', () => {
-//     if (card.classList.contains('shown')) {
-//       card.classList.remove('shown');
-//       if (favouritBtn.querySelector('.product-small-card__icon').classList.contains('is-active')) {
-//         favouritBtn.querySelector('.product-small-card__icon').classList.remove('product-small-card__icon--active');
-//       } else {
-//         card.classList.add('shown');
-//         favouritBtn.querySelector('.product-small-card__icon').classList.add('product-small-card__icon--active');
-//   }
-
-// }
-// }) 
-// })
 function initFavorite() {
-  const card = document.querySelector('.js-product');
-  const btn = card.querySelector('.js-favorite');
-  const heart = btn.querySelector('.js-favorite-icon');
+  const cards = document.querySelectorAll('.js-product');
 
-  const heartActive = () => {
-    console.log('b la');
-    if (heart.classList.contains('is-active')) {
-      heart.classList.remove('is-active');
-    } else {
-      heart.classList.add('is-active');
-    }
-  };
-  console.log('bla');
-  btn.addEventListener('click', heartActive);
+
+  cards.forEach(card => {
+    const favoriteBtn = card.querySelector('.js-favorite');
+    const heartIcon = favoriteBtn.querySelector('.js-favorite-icon');
+    favoriteBtn.classList.add('is-shown');
+
+
+    const pushFavoriteBtn = () => {
+      if (favoriteBtn.classList.contains('is-shown')) {
+        favoriteBtn.classList.remove('is-shown');
+        heartIcon.classList.add('is-active');
+      } else {
+        favoriteBtn.classList.add('is-shown');
+        heartIcon.classList.remove('is-active');
+        
+      };
+    };
+    
+    // heartIcon.classList.conains('is-active');
+    favoriteBtn.addEventListener('click', pushFavoriteBtn);
+  })
+
+
+
+
+
 };
+
 
 module.exports = initFavorite;
 
--- src/js/components/products.js
+++ src/js/components/products.js
@@ -1,17 +1,18 @@
 function productsOpen () {
 const productsSection = document.querySelectorAll('.js-products');
 
+
 productsSection.forEach(section => {
   const btnOpen = section.querySelector('.js-products-open');
   const products = section.querySelectorAll('.js-product');
   const iconArrow = btnOpen.querySelector('.products-preview__icon');
 
   btnOpen.addEventListener('click', () => {
-    if (section.classList.contains('is-show')) {
-      section.classList.remove('is-show');
+    if (section.classList.contains('is-shown')) {
+      section.classList.remove('is-shown');
       btnOpen.querySelector('span').textContent = 'Все акции';
-      if (iconArrow.classList.contains('products-preview__icon--rotate')) {
-        iconArrow.classList.remove('products-preview__icon--rotate');
+      if (iconArrow.classList.contains('js-products-icon-rotate')) {
+        iconArrow.classList.remove('js-products-icon-rotate');
       }
       
       products.forEach((product, index) => {
@@ -21,9 +22,9 @@ productsSection.forEach(section => {
         
       })
     } else {
-      section.classList.add('is-show');
+      section.classList.add('is-shown');
       btnOpen.querySelector('span').textContent = 'Скрыть';
-      iconArrow.classList.add('products-preview__icon--rotate');
+      iconArrow.classList.add('js-products-icon-rotate');
       products.forEach(product => {
         product.classList.remove('hidden')
       })
